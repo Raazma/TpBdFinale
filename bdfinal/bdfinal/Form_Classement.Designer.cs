2f54787c879b55e74a0ae4810e4b8d0a88cd5260
Balnian <super_f_qc@hotmail.com>
2014-12-11 10:43:03 -0500

    Ajout et mise en forme du formulaire Classement

---
+++ bdfinal/bdfinal/Form_Classement.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace bdfinal
+{
+    partial class Form_Classement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.CB_Classement = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.CB_Classement);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(856, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 100);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(856, 398);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // CB_Classement
+            // 
+            this.CB_Classement.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CB_Classement.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CB_Classement.FormattingEnabled = true;
+            this.CB_Classement.Items.AddRange(new object[] {
+            "Équipes",
+            "Joueurs",
+            "3 Meilleurs"});
+            this.CB_Classement.Location = new System.Drawing.Point(85, 12);
+            this.CB_Classement.Name = "CB_Classement";
+            this.CB_Classement.Size = new System.Drawing.Size(182, 24);
+            this.CB_Classement.TabIndex = 0;
+            // 
+            // Form_Classement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(856, 498);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form_Classement";
+            this.Text = "Classements";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox CB_Classement;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
