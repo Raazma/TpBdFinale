c4c12d7a975fbf4795863e8636396006f098c684
fred paquette <frederic_paquette@hotmail.com>
2014-12-14 12:38:33 -0400

    debut form ajout match

---
+++ bdfinal/bdfinal/Ajout_Match.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+namespace bdfinal
+{
+    public partial class Ajout_Match : Form
+    {
+        public OracleConnection oracon = new OracleConnection();
+        public Ajout_Match(OracleConnection oraconn)
+        {
+            InitializeComponent();
+            oracon = oraconn;
+            Fillbox();
+        }
+
+        private void Fillbox()
+        { 
+        string commande = "SELECT NOMEQUIPE FROM EQUIPE";
+            
+            OracleCommand oraclecomm = new OracleCommand(commande, oracon);
+            oraclecomm.CommandType = CommandType.Text;
+            OracleDataReader oraread = oraclecomm.ExecuteReader();
+            while (oraread.Read())
+            {
+                string ligne = oraread.GetString(0);
+                
+                 Cb_equipeR.Items.Add(ligne);
+                
+                   Cb_EquipeV.Items.Add(ligne);
+            }
+            oraread.Close();
+        }
+        
+        }
+       
+    }
+}
